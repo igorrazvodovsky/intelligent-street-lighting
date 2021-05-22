21f5efb89cb46483812005aef244fee1d16ef078
Igor Razvodovsky <igor.razvodovsky@gmail.com>
2021-05-22 21:37:43 +0300

    Make minor adjustments

--- src/app/management/dashboard/dashboard.component.ts
+++ src/app/management/dashboard/dashboard.component.ts
@@ -15,26 +15,8 @@ export class DashboardComponent implements OnInit {
   token: Observable<string>;
   modules: string[];
 
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  number: number;
+  tweenedNumber: number;
 
   constructor(
     private route: ActivatedRoute,
