de98c0dd1c48f5b35569bf93ab2da81c6fb3fb13
Igor Razvodovsky <igor.razvodovsky@gmail.com>
2021-08-26 21:45:45 +0300

    SC: add contactor controls settings

--- src/app/management/devices/device-detail/device-sc/device-sc.component.ts
+++ src/app/management/devices/device-detail/device-sc/device-sc.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task, Device, DeviceEvent } from '~local/types'
 
-
 @Component({
   selector: 'device-sc',
   templateUrl: './device-sc.component.html',
@@ -11,6 +10,46 @@ export class DeviceScComponent implements OnInit {
   @Input() device!: Device;
   on = true
   powerlines = true
+
+  relays = [
+    {
+      name: "RO1",
+      profile: "Default",
+      settings: {
+        on: true,
+        manual: false,
+        inverted: false
+      }
+    },
+        {
+      name: "RO2",
+      profile: "Default",
+      settings: {
+        on: true,
+        manual: true,
+        inverted: false
+      }
+    },
+            {
+      name: "RO3",
+      profile: "Default",
+      settings: {
+        on: true,
+        manual: false,
+        inverted: false
+      }
+    },
+                {
+      name: "RO4",
+      profile: "Default",
+      settings: {
+        on: false,
+        manual: false,
+        inverted: true
+      }
+    }
+
+]
 
   constructor() { }
 
