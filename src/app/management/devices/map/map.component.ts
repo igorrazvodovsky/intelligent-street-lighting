1c0b36d57c6aab6dac94c5f5fa0e34bcbdf6d79f
Igor Razvodovsky <igor.razvodovsky@gmail.com>
2021-08-31 22:18:37 +0300

    Map: add basic layout for profile layer markers

--- src/app/management/devices/map/map.component.ts
+++ src/app/management/devices/map/map.component.ts
@@ -21,7 +21,7 @@ export class MapComponent implements AfterViewInit, OnInit {
   devices: any;
   markersGeoJsonData: any;
   accessToken = 'pk.eyJ1IjoiaWdvcnJhenZvZG92c2t5IiwiYSI6ImNrczV3dHI3ODA1YTQycnF5bnV4N2xjcm0ifQ.1b4VIA7aqOZc_oiiTyNl-w';
-  deviceLayer = 'sc'
+  deviceLayer = 'profile'
 
   // TODO: Replace with something
   iconSC = '<svg width="24" height="24" fill="inherit" xmlns="http://www.w3.org/2000/svg"><path d="M20 11h-4.25V6a.75.75 0 10-1.5 0v5H4a2 2 0 00-2 2v6a2 2 0 002 2h16a2 2 0 002-2v-6a2 2 0 00-2-2zm.5 8a.5.5 0 01-.5.5H4a.5.5 0 01-.5-.5v-6a.5.5 0 01.5-.5h16a.501.501 0 01.5.5v6zM7 15a1 1 0 100 2.001A1 1 0 007 15zm5.702-6.702a3.249 3.249 0 010-4.596l-1.06-1.06a4.748 4.748 0 000 6.716l1.06-1.06zm5.656 1.06a4.748 4.748 0 000-6.717l-1.06 1.061a3.25 3.25 0 010 4.596l1.06 1.06z" fill="inherit"/></svg>'
@@ -67,6 +67,12 @@ export class MapComponent implements AfterViewInit, OnInit {
     });
   }
 
+  makeProfileMarker(clusterMarkers, childCount) {
+    return L.divIcon({
+      className: 'marker--cluster ' + status, html: `<div>${childCount} <i class="dot"></i><i class="dot"></i><i class="dot"></i></div>`
+    });
+  }
+
   makeStatusMarker(clusterMarkers, childCount) {
     let status = "active"
     let icon = ""
@@ -93,6 +99,7 @@ export class MapComponent implements AfterViewInit, OnInit {
         const clusterMarkers = cluster.getAllChildMarkers()
         if (this.deviceLayer == "status") return this.makeStatusMarker(clusterMarkers, cluster.getChildCount())
         if (this.deviceLayer == "sc") return this.makeSCMarker(clusterMarkers, cluster.getChildCount())
+        if (this.deviceLayer == "profile") return this.makeProfileMarker(clusterMarkers, cluster.getChildCount())
       }
     });
 
